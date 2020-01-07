35d0cd3b3082dd795037c6b2536552fcc55c98cc
leandro souza <leandro.dasilvati7@gmail.com>
2020-01-07 00:12:18 -0300

    keeping on course of vue- class 6

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 var hello = new Vue({
     el: '#hello',
     data:{
-        msg: 'Hello Vue!'
+        msg: 'Hallo Vue!',
+        peoples:[
+            {name:"Maria"},
+            {name:"Leandro"},
+            {name:"José"},
+            {name:"Thomas"},
+            {name:"EU mesmo"}
+        ],
+        newElement:'',
+        elements: []
+    },
+    methods:{
+        addElement: function(e){
+            let title = this.newElement.trim();
+            if (title) {
+                this.elements.push({title:title});
+                this.newElement = "";
+            }
+        },
+        removeElement: function(e, index) {
+            e.preventDefault();
+            this.elements.splice(index, 1);
+        }
     }
 });
