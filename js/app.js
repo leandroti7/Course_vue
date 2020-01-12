68b1eaa86f49ed0055f76dc6abd1bcf9d35a280a
leandro souza <leandro.dasilvati7@gmail.com>
2020-01-12 11:53:49 -0300

    atualizando projeto vue

--- js/app.js
+++ js/app.js
@@ -1,44 +1,17 @@
-var hello = new Vue({
-    el: '#hello',
-    data:{
-        msg: 'Hallo Vue!',
-        peoples:[
-            {name:"Maria"},
-            {name:"Leandro"},
-            {name:"José"},
-            {name:"Thomas"},
-            {name:"EU mesmo"}
-        ],
-        newElement:'',
-        elements: [],
-        objectA:{
-            'font-size':'30px'
-        },
-        objectB:{
-            color:"red"
-        },
-        myListForm: [],
-        myForm:{
-            name:'',
-            email:''
-        }
+var app = new Vue({
+    el: '#app',
+    data: {
+        books:[],
+        mySearch:''
     },
-    methods:{
-        addElement: function(e){
-            let title = this.newElement.trim();
-            if (title) {
-                this.elements.push({title:title});
-                this.newElement = "";
-            }
-        },
-        removeElement: function(e, index) {
-            e.preventDefault();
-            this.elements.splice(index, 1);
-        },
-        addForm: function (){
-            this.myListForm.push({name: this.myForm.name, email: this.myForm.email});
-            this.myForm.name = "";
-            this.myForm.email = "";
-        }
-    }
+    methods: {
+       
+    },
+    created:function(){
+        let self = this;
+        self.$http.get('dataSever.json').then(function(response){
+            // console.log(response);
+            self.books = response.data;
+        });
+    },
 });
